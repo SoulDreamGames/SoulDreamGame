d7306bdfbe7f9d42f10c9bf2d32ceba873bfb7ee
pbayona <pbayonag@hotmail.com>
2023-04-21 17:20:31 +0200

    Fixed bug on respawning at second

--- Assets/Scripts/GameManager/TimeManager.cs
+++ Assets/Scripts/GameManager/TimeManager.cs
@@ -9,7 +9,7 @@ using GameEventType = GameManager.GameEventType;
 using GameState = GameManager.GameState;
 
 
-public class TimeManager : MonoBehaviour
+public class TimeManager : MonoBehaviourPunCallbacks, IPunObservable
 {
     // this time only matters when staying at OnWave state
     //ToDo: if every enemy on this wave has dead, change this state to WaittingWave and reset time to that
@@ -37,9 +37,6 @@ public class TimeManager : MonoBehaviour
     
     //GameManager
     private GameManager _gameManager;
-    
-    //PUN Properties
-    private Room _room;
 
 
     public void Initialize(GameManager gameManager)
@@ -57,18 +54,6 @@ public class TimeManager : MonoBehaviour
         foreach (var init in gameStateTimes)
         {
             _gameStateTimes[init.gameState] = init.time;
-        }
-        
-        //Init time and set current time state
-        _room = PhotonNetwork.CurrentRoom;
-        if (PhotonNetwork.IsMasterClient)
-        {
-            Hashtable hash = new Hashtable() { { "Time", _time } };
-            _room.SetCustomProperties(hash);
-        }
-        else
-        {
-            _time = (double)_room.CustomProperties["Time"];
         }
         
         _gameManager.SubscribeToEvent(GameEventType.onTimerUpdated, SetUITimer);
@@ -94,8 +79,6 @@ public class TimeManager : MonoBehaviour
         
         if (PhotonNetwork.IsMasterClient)
             UpdateTimer();
-
-        else ReadTimer();
     }
     
     void SetUITimer()
@@ -109,36 +92,28 @@ public class TimeManager : MonoBehaviour
     {
         //Update Room Time
         _time -= Time.deltaTime;
-        
-        UpdateRoomTimeProperty();
-        
+
         if (!(_time <= 0f)) return;
 
         //If game time ended, check current state to start next one
-
         _gameManager.UpdateGameState();
         _time = _gameStateTimes[_gameManager.currentGameState];
-    }
-
-    //Read timer for non-master clients / non-server
-    void ReadTimer()
-    {
-        _time = (double)_room.CustomProperties["Time"];
-    }
-    
-    //Update timer for all clients
-    void UpdateRoomTimeProperty()
-    {
-        Hashtable hash = _room.CustomProperties;
-        hash.Remove("Time");
-        hash.Add("Time", _time);
-
-        if (PhotonNetwork.NetworkClientState.Equals(ClientState.Leaving)) return;
-        _room.SetCustomProperties(hash);
     }
 
     public float GetStateTime(GameState state)
     {
         return _gameStateTimes[_gameManager.currentGameState];
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(_time);
+        }
+        else if (stream.IsReading)
+        {
+            _time = (double)stream.ReceiveNext();
+        }
     }
 }
